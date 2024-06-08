6e547149c5b97504072db8559c9c5bf16752e89e
asus test <msa-99@outlook.com>
2024-06-09 01:07:23 +0600

    O(sqrt(N)) complexity

---
+++ O_sqrt(N)_complexity.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // O(sqrt(N)) complexity divisor ber kort use kora hoy.
+    // O(sqrt(N)) complexity 1 sec e 10^14 step complte korte pare. sqrt(10^14)=10^7.
+    return 0;
+}
