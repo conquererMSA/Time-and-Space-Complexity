f2286393c4f909e77f561ad8f5cceeb93be280f8
asus test <msa-99@outlook.com>
2024-06-09 00:40:59 +0600

    O(N*N) / O(N*M) complexity

---
+++ O_N_N_complexity.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // nested loop er kkhetre O(N*N) ba O(N*M) => 10^3 porzonto loop colbe.
+    // 2D array and selection sort e nested loop use kora hoy.
+    // 10^3 3r square hobe 10^6 kintu 10^4 er square value hoy 10^8, tai O(N*N) complexity te 1 sec e 10^3 ba 1000 step complete kora zay.
+    return 0;
+}
