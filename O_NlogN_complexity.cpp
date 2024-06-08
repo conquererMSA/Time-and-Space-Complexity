7a4429f998a88a91880863e4fb9d71e4f3e44a66
asus test <msa-99@outlook.com>
2024-06-09 01:03:02 +0600

    O(NlogN) complexity

---
+++ O_NlogN_complexity.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //O(NlogN) complexity te 1sec e 10^5 step complete kora zay. 2 sec e 10^6 step complete kora zay.
+    // sort() function O(NlogN) complexity use kore.
+    // array er size 10^6 hole sort function kaj korbe. karon O(NlogN) complexity 2 sec e 10^6 step complete korte pare. 
+    return 0;
+}
