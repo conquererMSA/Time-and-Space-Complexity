c487d618d44ac39347e3f63ae8f113ab19bdfcd5
asus test <msa-99@outlook.com>
2024-06-08 22:53:49 +0600

    rules of timeComplexity

--- timeComplexity.cpp
+++ timeComplexity.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    /*
+    Time Complexity:
+    1. Time complexity programe run hote kototuku somoy niche tar upor nirvor kore na. Complexity nirvor kore koyta step e programme shesh hoyeche tar upor.
+    2. Loop run e complexity beshi hoy. Constant complexity o(1) hisab kora hoy na.
+    3. Time complexity input valu er upor nirvar kore.
+    4. Ekti function er onek gulu complexity'r moddhye maximum complxity hocce tar total complexity.
+    5. Complxity te worst case ke calculate kora hoy.
+    6. Kuno function er Constant value zemon n/2 ba n/3 er moddhye 2/3 bad diye n ke oi function er complexity hisab kora hoy.
+    */
     return 0;
 }
