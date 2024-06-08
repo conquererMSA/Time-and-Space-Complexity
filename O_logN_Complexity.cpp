20f20b944f91e9f464b9375766c38a360ec3fed6
asus test <msa-99@outlook.com>
2024-06-09 00:08:10 +0600

    O(logN) complexity

--- O_logN_Complexity.cpp
+++ O_logN_Complexity.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin>>n;
+    // while(n>0){ //O(logN)
+    //     int digit=n%10;
+    //     cout<<digit<<endl;
+    //     n=n/10;
+    // }
+    for(int i=0; i<n; i=i*2){ //O(logN)
+        cout<<i<<endl;
+    }
+    // Multiplication and division er khetre O(logN) complexity hoy.
+    //O(logN) complexity O(N) er ceye better performance kore.
+    //O(logN) complexity te 10^18 er jonno 60 ti step complete korte hoy.
     return 0;
 }
